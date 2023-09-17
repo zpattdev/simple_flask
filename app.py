125ac5a4c5137aa3fa74ab9da790fb40d6cc4d0d
zpattdev <zpattdev@gmail.com>
2023-09-17 11:46:50 -0500

    app uses db config and queries Person model

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 # app.py
 
 from flask import render_template
-import connexion
+import config
+from models import Person
 
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
-
-@app.route("/hello")
-def hello():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
