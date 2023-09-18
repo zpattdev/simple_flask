ab43991ab984e21c2ee1699a7128f00fcb2d5640
zack <zpattdev@gmail.com>
2023-09-18 09:16:17 -0400

    add Note table to db model

--- models.py
+++ models.py
@@ -4,12 +4,27 @@ from datetime import datetime
 from config import db, ma
 
 
+class Note(db.Model):
+    __tablename__ = "note"
+    id = db.Column(db.Integer, primary_key=True)
+    person_id = db.Column(db.Integer, db.ForeignKey("person.id"))
+    content = db.Column(db.String, nullable=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
 class Person(db.Model):
     __tablename__ = "person"
     id = db.Column(db.Integer, primary_key=True)
     lname = db.Column(db.String(32), unique=True)
     fname = db.Column(db.String(32))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    note = db.relationship(
+        Note,
+        backref="person",
+        cascade="all, delete, delete-orphan",
+        single_parent=True,
+        order_by="desc(Note.timestamp)"
+    )
 
 class PersonSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
