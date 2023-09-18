3a5c29a7f48f239412baafc958aeec6a4a70eb42
zack <zpattdev@gmail.com>
2023-09-18 16:08:17 -0400

    add update and delete ops to api and notes.py

--- notes.py
+++ notes.py
@@ -13,3 +13,25 @@ def read_one(note_id):
         return note_schema.dump(note)
     else:
         abort(404, f"Note with ID {note_id} not found")
+
+def update(note_id, note):
+    existing = Note.query.get(note_id)
+
+    if existing:
+        update_note = note_schema.load(note, session=db.session)
+        existing.content = update_note.content
+        db.session.merge(existing)
+        db.session.commit()
+        return note_schema.dump(existing)
+    else:
+        abort(404, f"Note with ID {note_id} not found")
+
+def delete(note_id):
+    existing = Note.query.get(note_id)
+
+    if existing:
+        db.session.delete(existing)
+        db.session.commit()
+        return make_response(f"{note_id} successfully deleted", 204)
+    else:
+        abort(404, f"Note with ID {note_id} not found")
