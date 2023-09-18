b70da601c9652ef274e4a10529c98c1aeb1bfc40
zack <zpattdev@gmail.com>
2023-09-18 17:41:32 -0400

    add create op to api and notes model

--- notes.py
+++ notes.py
@@ -3,7 +3,7 @@
 from flask import abort, make_response
 
 from config import db
-from models import Note, note_schema
+from models import Note, Person, note_schema
 
 
 def read_one(note_id):
@@ -35,3 +35,15 @@ def delete(note_id):
         return make_response(f"{note_id} successfully deleted", 204)
     else:
         abort(404, f"Note with ID {note_id} not found")
+
+def create(note):
+    person_id = note.get("person_id")
+    person = Person.query.get(person_id)
+
+    if person:
+        new_note = note_schema.load(note, session=db.session)
+        person.notes.append(new_note)
+        db.session.commit()
+        return note_schema.dump(new_note), 201
+    else:
+        abort(404, f"Person not found for ID: {person_id} not found")
