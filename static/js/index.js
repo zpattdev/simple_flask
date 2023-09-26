1e24c82d6b622e74b83ce35201bedc38a9f7c309
zack <zpattdev@gmail.com>
2023-09-26 18:32:34 -0400

    add create person functionality

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,10 @@
 // static/js/index.js
 
 import { DebugForm } from "./debug.js";
+import { People } from "./people.js";
 
 function main() {
+  new People();
   if (document.querySelector(".debug-card")) {
     const debug = new DebugForm();
     debug.showResponse("");
