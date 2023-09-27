fbe9cba75a060b807ecad5b0e63bc1fc9ff52941
zack <zpattdev@gmail.com>
2023-09-26 20:27:06 -0400

    add js to create notes

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,11 @@
 
 import { DebugForm } from "./debug.js";
 import { People } from "./people.js";
+import { Notes } from "./notes.js";
 
 function main() {
   new People();
+  new Notes();
   if (document.querySelector(".debug-card")) {
     const debug = new DebugForm();
     debug.showResponse("");
